6a3557def8358dd04f7815dc5f4dba78cef86185
Krzysiek <krzysiek.dziedzic@outlook.com>
2018-01-30 12:11:38 +0100

    Accuracy with change train data- plot

--- ModelPerformance.py
+++ ModelPerformance.py
@@ -110,3 +110,96 @@ tt7 = time()
 print("Time elapsed: ", tt7 - tt6)
 print("\n")
 
+
+# ANALYSIS
+
+list1=[]
+list2=[]
+list3=[]
+
+list4=[]
+list5=[]
+
+list6=[]
+list7=[]
+
+list8=[]
+list9=[]
+
+for i in range(0,100,5):
+    X_train1, X_test1, y_train1, y_test1 = train_test_split(X_train, y_train, test_size=0.049+i/100.0, random_state=0)
+
+    # Linear Regression
+    t0 = time()
+    lr = linear_model.LinearRegression()
+    clf_lr = lr.fit(X_test1, y_test1)
+    accuracy_lr = clf_lr.score(X_test, y_test)
+    t1 = time()
+    timeElapsed_lr=t1-t0
+    list1.append(i)
+    list2.append(accuracy_lr)
+    list3.append(timeElapsed_lr)
+
+    # Random Forest
+    t2 = time()
+    rf = RandomForestClassifier(n_estimators=100, n_jobs=-1)
+    clf_rf = rf.fit(X_test1, y_test1)
+    accuracy_rf = clf_rf.score(X_test, y_test)
+    t3 = time()
+    timeElapsed_rf = t3 - t2
+    list4.append(accuracy_rf)
+    list5.append(timeElapsed_rf)
+
+    # KNN
+    t4 = time()
+    knn = KNeighborsClassifier()
+    clf_knn = knn.fit(X_test1, y_test1)
+    accuracy_knn = clf_knn.score(X_test, y_test)
+    t5 = time()
+    timeElapsed_knn = t5 - t4
+    list6.append(accuracy_knn)
+    list7.append(timeElapsed_knn)
+
+    # Naive Bayes
+    t6 = time()
+    nb = BernoulliNB()
+    clf_nb = nb.fit(X_test1, y_test1)
+    accuracy_nb = clf_nb.score(X_test, y_test)
+    t7 = time()
+    timeElapsed_nb = t7 - t6
+    list8.append(accuracy_nb)
+    list9.append(timeElapsed_nb)
+
+
+# view reliability diagram
+import matplotlib.pyplot as plt
+
+plt.figure(1)
+plt.subplot('211')
+plt.plot(list1, list2, label="Linear Regression")
+leg = plt.legend()
+leg_lines = leg.get_lines()
+leg_texts = leg.get_texts()
+plt.title('Accuracy with change train data')
+plt.xlabel("% test size")
+plt.ylabel("Accuracy")
+plt.grid(True)
+
+plt.subplot('212')
+plt.subplots_adjust(hspace=0.3)
+plt.plot(list1, list4, label="Random Forrest")
+plt.plot(list1, list6, label="KNN")
+plt.plot(list1, list8, label="Naive Bayes")
+leg = plt.legend()
+leg_lines = leg.get_lines()
+leg_texts = leg.get_texts()
+plt.title('Accuracy with change train data')
+plt.xlabel("% test size")
+plt.ylabel("Accuracy")
+plt.grid(True)
+
+
+
+plt.show()
+
+
