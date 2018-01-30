d7f311bb0f31bc84c6591f208e12e3239467b434
Krzysiek <krzysiek.dziedzic@outlook.com>
2018-01-30 13:59:14 +0100

    All plot (time/accuracy - train/test data with change size test data)

--- ModelPerformance.py
+++ ModelPerformance.py
@@ -1,7 +1,7 @@
 from pyspark.sql import SparkSession
 from sklearn.cross_validation import train_test_split
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.naive_bayes import BernoulliNB
+from sklearn.tree import DecisionTreeClassifier
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn import linear_model
 from sklearn.model_selection import cross_val_score
@@ -91,19 +91,19 @@ tt5 = time()
 print("Time elapsed: ", tt5 - tt4)
 print("\n")
 
-# Naive Bayes accuracy and time elapsed caculation
+# Decision Tree tree accuracy and time elapsed caculation
 t6 = time()
-print("NaiveBayes")
-nb = BernoulliNB()
-clf_nb = nb.fit(X_train, y_train)
-print("Acurracy: ", clf_nb.score(X_test, y_test))
+print("Decision Tree")
+dt = DecisionTreeClassifier(min_samples_split=20,random_state=99)
+clf_dt = dt.fit(X_train, y_train)
+print("Acurracy: ", clf_dt.score(X_test, y_test))
 t7 = time()
 print("Time elapsed: ", t7 - t6)
 
-# Cross-validation for Naive Bayes
+# Cross-validation for Decision Tree
 tt6 = time()
 print("Cross validation RESULT")
-scores = cross_val_score(nb, df[features], df[' shares'], cv=5)
+scores = cross_val_score(dt, df[features], df[' shares'], cv=5)
 print(scores)
 print(scores.mean())
 tt7 = time()
@@ -112,6 +112,7 @@ print("\n")
 
 
 # ANALYSIS
+# CHANGE TRAIN DATA
 
 list1=[]
 list2=[]
@@ -131,7 +132,6 @@ for i in range(0,100,5):
 
     # Linear Regression
     t0 = time()
-    lr = linear_model.LinearRegression()
     clf_lr = lr.fit(X_test1, y_test1)
     accuracy_lr = clf_lr.score(X_test, y_test)
     t1 = time()
@@ -142,7 +142,6 @@ for i in range(0,100,5):
 
     # Random Forest
     t2 = time()
-    rf = RandomForestClassifier(n_estimators=100, n_jobs=-1)
     clf_rf = rf.fit(X_test1, y_test1)
     accuracy_rf = clf_rf.score(X_test, y_test)
     t3 = time()
@@ -152,7 +151,6 @@ for i in range(0,100,5):
 
     # KNN
     t4 = time()
-    knn = KNeighborsClassifier()
     clf_knn = knn.fit(X_test1, y_test1)
     accuracy_knn = clf_knn.score(X_test, y_test)
     t5 = time()
@@ -160,15 +158,14 @@ for i in range(0,100,5):
     list6.append(accuracy_knn)
     list7.append(timeElapsed_knn)
 
-    # Naive Bayes
+    # Decision Tree
     t6 = time()
-    nb = BernoulliNB()
-    clf_nb = nb.fit(X_test1, y_test1)
-    accuracy_nb = clf_nb.score(X_test, y_test)
+    clf_dt = dt.fit(X_test1, y_test1)
+    accuracy_dt = clf_dt.score(X_test, y_test)
     t7 = time()
-    timeElapsed_nb = t7 - t6
-    list8.append(accuracy_nb)
-    list9.append(timeElapsed_nb)
+    timeElapsed_dt = t7 - t6
+    list8.append(accuracy_dt)
+    list9.append(timeElapsed_dt)
 
 
 # view reliability diagram
@@ -189,7 +186,7 @@ plt.subplot('212')
 plt.subplots_adjust(hspace=0.3)
 plt.plot(list1, list4, label="Random Forrest")
 plt.plot(list1, list6, label="KNN")
-plt.plot(list1, list8, label="Naive Bayes")
+plt.plot(list1, list8, label="Decision Tree")
 leg = plt.legend()
 leg_lines = leg.get_lines()
 leg_texts = leg.get_texts()
@@ -198,8 +195,117 @@ plt.xlabel("% test size")
 plt.ylabel("Accuracy")
 plt.grid(True)
 
+plt.figure(2)
+plt.subplots_adjust(hspace=0.3)
+plt.plot(list1, list3, label="Linear Regression")
+plt.plot(list1, list5, label="Random Forrest")
+plt.plot(list1, list7, label="KNN")
+plt.plot(list1, list9, label="Decision Tree")
+leg = plt.legend()
+leg_lines = leg.get_lines()
+leg_texts = leg.get_texts()
+plt.title('Time with change train data')
+plt.xlabel("% test size")
+plt.ylabel("Time")
+plt.grid(True)
 
+# CHANGE TEST DATA
+
+list1=[]
+list2=[]
+list3=[]
+
+list4=[]
+list5=[]
+
+list6=[]
+list7=[]
+
+list8=[]
+list9=[]
+
+for i in range(0,100,5):
+    X_train1, X_test1, y_train1, y_test1 = train_test_split(X_test, y_test, test_size=0.049+i/100.0, random_state=0)
+
+    # Linear Regression
+    t0 = time()
+    clf_lr = lr.fit(X_train,y_train)
+    accuracy_lr = clf_lr.score(X_test1, y_test1)
+    t1 = time()
+    timeElapsed_lr=t1-t0
+    list1.append(i)
+    list2.append(accuracy_lr)
+    list3.append(timeElapsed_lr)
+
+    # Random Forest
+    t2 = time()
+    clf_rf = rf.fit(X_train,y_train)
+    accuracy_rf = clf_rf.score(X_test1, y_test1)
+    t3 = time()
+    timeElapsed_rf = t3 - t2
+    list4.append(accuracy_rf)
+    list5.append(timeElapsed_rf)
+
+    # KNN
+    t4 = time()
+    clf_knn = knn.fit(X_train,y_train)
+    accuracy_knn = clf_knn.score(X_test1, y_test1)
+    t5 = time()
+    timeElapsed_knn = t5 - t4
+    list6.append(accuracy_knn)
+    list7.append(timeElapsed_knn)
+
+    # Decision Tree
+    t6 = time()
+    clf_dt = dt.fit(X_train,y_train)
+    accuracy_dt = clf_dt.score(X_test1, y_test1)
+    t7 = time()
+    timeElapsed_dt = t7 - t6
+    list8.append(accuracy_dt)
+    list9.append(timeElapsed_dt)
+
+
+# view reliability diagram
+import matplotlib.pyplot as plt
+
+plt.figure(3)
+plt.subplot('211')
+plt.plot(list1, list2, label="Linear Regression")
+leg = plt.legend()
+leg_lines = leg.get_lines()
+leg_texts = leg.get_texts()
+plt.title('Accuracy with change test data')
+plt.xlabel("% test size")
+plt.ylabel("Accuracy")
+plt.grid(True)
+
+plt.subplot('212')
+plt.subplots_adjust(hspace=0.3)
+plt.plot(list1, list4, label="Random Forrest")
+plt.plot(list1, list6, label="KNN")
+plt.plot(list1, list8, label="Decision Tree")
+leg = plt.legend()
+leg_lines = leg.get_lines()
+leg_texts = leg.get_texts()
+plt.title('Accuracy with change test data')
+plt.xlabel("% test size")
+plt.ylabel("Accuracy")
+plt.grid(True)
+
+plt.figure(4)
+plt.subplots_adjust(hspace=0.3)
+plt.plot(list1, list3, label="Linear Regression")
+plt.plot(list1, list5, label="Random Forrest")
+plt.plot(list1, list7, label="KNN")
+plt.plot(list1, list9, label="Decision Tree")
+leg = plt.legend()
+leg_lines = leg.get_lines()
+leg_texts = leg.get_texts()
+plt.title('Time with change test data')
+plt.xlabel("% test size")
+plt.ylabel("Time")
+plt.grid(True)
 
 plt.show()
 
-
+spark.stop
