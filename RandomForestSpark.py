2b7eb6fff1b504602c4ba92a5ae01c46fdc33952
Piotr Harasiuk <pharasiuk@gmail.com>
2018-02-03 11:13:25 +0100

    fix assign column and label

--- RandomForestSpark.py
+++ RandomForestSpark.py
@@ -33,7 +33,7 @@ if __name__ == "__main__":
         return df
 
     # Assign all column names to `columns`
-    columns = [' timedelta', ' n_tokens_title', ' n_tokens_content', ' n_unique_tokens', ' n_non_stop_words',
+    columns = [' n_tokens_title', ' n_tokens_content', ' n_unique_tokens', ' n_non_stop_words',
                ' n_non_stop_unique_tokens', ' num_hrefs', ' num_self_hrefs', ' num_imgs', ' num_videos',
                ' average_token_length', ' num_keywords', ' data_channel_is_lifestyle', ' data_channel_is_entertainment',
                ' data_channel_is_bus', ' data_channel_is_socmed', ' data_channel_is_tech', ' data_channel_is_world',
@@ -41,7 +41,7 @@ if __name__ == "__main__":
                ' kw_max_avg', ' kw_avg_avg', ' self_reference_min_shares', ' self_reference_max_shares',
                ' self_reference_avg_sharess', ' weekday_is_monday', ' weekday_is_tuesday', ' weekday_is_wednesday',
                ' weekday_is_thursday', ' weekday_is_friday', ' weekday_is_saturday', ' weekday_is_sunday',
-               ' is_weekend', ' LDA_00', ' LDA_01', ' LDA_02', ' LDA_03', ' LDA_04', ' global_subjectivity',
+               ' LDA_00', ' LDA_01', ' LDA_02', ' LDA_03', ' LDA_04', ' global_subjectivity',
                ' global_sentiment_polarity', ' global_rate_positive_words', ' global_rate_negative_words',
                ' rate_positive_words', ' rate_negative_words', ' avg_positive_polarity', ' min_positive_polarity',
                ' max_positive_polarity', ' avg_negative_polarity', ' min_negative_polarity', ' max_negative_polarity',
@@ -50,13 +50,10 @@ if __name__ == "__main__":
 
     # Conver the `df` columns to `FloatType()`
     data = convertColumn(data, columns, FloatType())
-
-    data = data.select(' shares', ' num_keywords', ' data_channel_is_lifestyle', ' data_channel_is_entertainment',
-               ' data_channel_is_bus', ' data_channel_is_socmed', ' data_channel_is_tech', ' data_channel_is_world',
-               ' kw_min_min', ' kw_max_min')
+    data = data.select(columns).dropDuplicates()
 
     # Define the `input_data`
-    input_data = data.rdd.map(lambda x: (1 if x[0] < 2000 else 0, DenseVector(x[1:])))
+    input_data = data.rdd.map(lambda x: ('0' if int(x[-1]) < 2000.0 else '1', DenseVector(x[0:-1])))
 
     # Replace `data` with the new DataFrame
     data = spark.createDataFrame(input_data, ["label", "features"])
@@ -94,7 +91,8 @@ if __name__ == "__main__":
     predictions.select("predictedLabel", "label", "features").show(100)
 
     # Select (prediction, true label) and compute test error
-    evaluator = MulticlassClassificationEvaluator(labelCol="indexedLabel", predictionCol="prediction", metricName="accuracy")
+    evaluator = MulticlassClassificationEvaluator(labelCol="indexedLabel", predictionCol="prediction"
+                                                  , metricName="accuracy")
     accuracy = evaluator.evaluate(predictions)
     print("Test Error = %g" % (1.0 - accuracy))
 
